5c5baf85c870507ffba5864158be0eb1c9d76e24
Aaron Harabedian <aaronharabedian@gmail.com>
2024-01-24 19:37:41 -0500

    Added content

---
+++ external/versions.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = "~> 1.6.0"
+
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "aharabedian"
+
+    workspaces {
+      name = "homelab-external"
+    }
+  }
+
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.11.0"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.9.0"
+    }
+
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.2.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  email   = var.cloudflare_email
+  api_key = var.cloudflare_api_key
+}
+
+provider "kubernetes" {
+  config_path = "../metal/kubeconfig.yaml"
+}
